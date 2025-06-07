96eb7e3179baf3f4ed156d90dcf6441208dfe79f
James Coliz <jcoliz@outlook.com>
2025-06-07 10:10:51 -0700

    build: Set all logs to systemd format

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,8 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.MsSentinel_BanffProtect_BackEnd>("backend");
+var apiService = builder.AddProject<Projects.MsSentinel_BanffProtect_BackEnd>("backend")
+    .WithEnvironment("Logging__Console__FormatterName","systemd");
 
 builder.AddProject<Projects.MsSentinel_BanffProtect_FrontEnd_Blazor>("frontend-blazor")
+    .WithEnvironment("Logging__Console__FormatterName","systemd")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
 
